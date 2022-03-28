ba3582bb7c2aab068f3270e47e8acd952fed93d0
Yakub Ali <mamuzaan@yahoo.com>
2022-03-28 13:41:03 +0000

    debugg

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ def print_board(board, name):
     print ("-------------------------------")
     print (name + "'s board")
     for row in board:
-        print " ".join(row)
+        print (" ".join(row))
 
 
 def random_num(board):
@@ -56,24 +56,23 @@ def take_input_and_play_game():
 
 def play_game():
     if (user_guess_row < 0 or user_guess_row > grid_size) or (user_guess_col < 0 or user_guess_col > grid_size):
-        print "Oops, that's not even in the ocean. Guess again:"
+        print ("Oops, that's not even in the ocean. Guess again:")
         take_input_and_play_game()
     else:
         show_result()
 
 
 def show_result():
-    print "Player guessed: (" + str(user_guess_row) + "," + str(user_guess_col) + ")"
+    print ("Player guessed: (" + str(user_guess_row) + "," + str(user_guess_col) + ")")
     if user_guess_row == computer_ship_row and user_guess_col == computer_ship_col:
-        print "Congratulations!" + user_name + "win"
+        print ("Congratulations!" + user_name + "win")
     else:
-        print user_name + " missed this time."
+        print (user_name + " missed this time.")
 
-    print "Computer guessed: (" + str(computer_guess_row) + "," + str(user_guess_col) + ")"
+    print ("Computer guessed: (" + str(computer_guess_row) + "," + str(user_guess_col) + ")")
     if computer_guess_row == computer_ship_row and computer_guess_col == computer_ship_col:
-        print "Congratulations!" + "computer win"
+        print ("Congratulations!" + "computer win")
     else:
         print "Computer missed this time."
-
 
 take_input_and_play_game()
