04531d8c8f4a671e6b945d8338207ffbcee9f86b
Yakub Ali <mamuzaan@yahoo.com>
2022-03-28 10:41:12 +0000

    declare variable user_board and computer_board and for loop for grid size board and print_board function

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Import random go to generate random int in game
+"""
+
+from random import randint 
+
+# Two Player one is user_name and another is Computer
+user_name = raw_input("Please enter your name: ")
+Computer_name = "Computer"
+
+# user can create grid size self
+grid_size = int(input("Please Enter your grid size: "))
